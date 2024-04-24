84714706a4cfb5a5a006336ed3c5e4129feb88d8
thiagoconsult <thiago.consultx@gmail.com>
2024-04-24 19:40:31 -0300

    Criação do projeto clone iluvcoffee

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+
+@Controller('coffees')
+export class CoffeesController {
+    @Get()
+    findAll() {
+        return 'This action returns all coffees.'
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return `This action return #${id} coffee.`
+    }
+
+    @Post()
+    create(@Body() body) {
+        return body
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body) {
+        return `This action updates #${id} coffee.`
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return `This action removes #${id} coffee.`
+    }
+}
